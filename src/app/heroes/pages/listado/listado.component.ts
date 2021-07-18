6db27c68eaae7de6069ebd2282c54b94755d8100
Juan David Barrero <=>
2021-07-18 16:17:29 -0500

    Cards with image added

--- src/app/heroes/pages/listado/listado.component.ts
+++ src/app/heroes/pages/listado/listado.component.ts
@@ -1,16 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { Heroe } from '../../interfaces/heroes.interface';
+import { HeroesService } from '../../services/heroes.service';
 
 @Component({
   selector: 'app-listado',
   templateUrl: './listado.component.html',
-  styles: [
-  ]
+  styles: [`
+    mat-card{
+      margin-top:20px;
+      border-radius:10px;
+    }
+  `]
 })
 export class ListadoComponent implements OnInit {
 
-  constructor() { }
+  heroes: Heroe[] = [];
+
+  constructor(private heroeService:HeroesService) { }
 
   ngOnInit(): void {
+    this.heroeService.getHeroes().subscribe( heroe => this.heroes = heroe);
   }
 
 }
---
+++ src/app/heroes/services/heroes.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/internal/Observable';
+
+import { Heroe } from '../interfaces/heroes.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class HeroesService {
+
+  constructor(private http: HttpClient) { }
+
+  getHeroes(): Observable<Heroe[]> {
+    return this.http.get<Heroe[]>('http://localhost:3000/heroes')
+  }
+
+
+}
